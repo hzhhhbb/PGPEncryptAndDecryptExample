3c6d8928e851737220fd9e6f703cbfdc38a16dcc
vincent <huangzihengengineer@gmail.com>
2019-12-18 15:18:58 +0800

    Add project files.

---
+++ PGP/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using PGP.PGP;
+
+namespace PGP
+{
+    class Program
+    {
+         static void Main(
+            string[] args)
+        {
+            // 加密文档
+            KeyBasedFileProcessor.EncryptFile(@"EncryptedTest.txt", @"test.txt", @"PublicKey.asc", false,true);
+            Console.WriteLine("已加密");
+
+            KeyBasedFileProcessor.DecryptFile(@"EncryptedTest.txt", @"PrivateKey.asc",new Char[]{'1','2','3'}, @"DecryptedTest.txt");
+            Console.WriteLine("已解密");
+            Console.ReadKey();
+        }
+    }
+}
+
